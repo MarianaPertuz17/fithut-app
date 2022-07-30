234930fcc81eaa457bf769d7333c59dc503029fa
Mariana <marianapertuzv@gmail.com>
2022-07-30 10:59:22 -0500

    feat: added linting and gitignore files

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 
 exports.getUser = async (req, res) => {
   try {
-    const response = await User.find(); //hay que cambiar por findById
+    const response = await User.findById({_id: '62de1ccafceacff8700e6722'}); //hay que cambiar por findById
     res.status(200).send({res: response, error: false});
   } catch (e) {
     res.status(500).send({res: 'Server error', error: true});
@@ -25,7 +25,7 @@ exports.postUser = async (req, res) => {
 exports.putEquipment = async (req, res) => {
   try {
     const { equipment } = req.body;
-    const updatedUser = await User.findOneAndUpdate({id : req.params.id}, {$set: {equipment: equipment}})
+    const updatedUser = await User.findOneAndUpdate({id : req.params.id}, {$set: {equipment: equipment}}, {new: true})
     res.status(201).send({res: equipment , error: false});
   } catch (e) {
     res.status(500).send({res: 'Cound not update equipment for user', error: true});
